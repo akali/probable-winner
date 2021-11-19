7fc34300f3fe36b6f9dd39d19c29622c6646571f
KillerPain <emailforanypurposes@gmail.com>
2021-11-19 18:12:25 +0600

    lecture-5

--- applications/healtcare/src/app/list/list/list.component.ts
+++ applications/healtcare/src/app/list/list/list.component.ts
@@ -1,12 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  items,
+  ListItem,
+} from 'applications/healtcare/src/models/list-item.model';
+import { Subject } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  takeUntil,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'hc-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListComponent implements OnInit {
-  constructor() {}
+export class ListComponent implements OnInit, OnDestroy {
+  listItems = [...items];
 
-  ngOnInit(): void {}
+  searchFormControl = new FormControl('');
+
+  destroyed = new Subject<boolean>();
+
+  constructor(private _changeRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    setInterval(() => {}, 1000);
+    this.searchFormControl.valueChanges
+      .pipe(
+        takeUntil(this.destroyed),
+        debounceTime(250),
+        distinctUntilChanged(),
+        map(this.mapToArr),
+        map((val) => this.mapToItems(val))
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next(true);
+  }
+
+  getItems(): ListItem[] {
+    console.log('here');
+    return this.listItems;
+  }
+
+  mapToArr(value: string): ListItem[] {
+    const arr = items.filter((item) =>
+      item.Name.toLowerCase().includes(value.toLowerCase())
+    );
+    return arr;
+  }
+
+  mapToItems(value: ListItem[]): ListItem[] {
+    this.listItems = [...value];
+    this._changeRef.markForCheck();
+    return this.listItems;
+  }
 }
---
+++ applications/healtcare/src/models/list-item.model.ts
@@ -0,0 +1,131 @@
+export class ListItem {
+  Name!: string;
+  Description!: string;
+
+  constructor(item: ListItem) {
+    Object.assign(this, item);
+  }
+}
+
+export const items = [
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 1',
+    Description: 'hello',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+  new ListItem({
+    Name: 'Продукт 2',
+    Description: 'how are you',
+  }),
+];
